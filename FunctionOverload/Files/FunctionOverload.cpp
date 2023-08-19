7cf24e220cce9c9009997e8cd7ac6fdf494ae789
Danielle <110608265+kentnagum@users.noreply.github.com>
2023-08-19 21:38:58 +0800

    Add files via upload

---
+++ FunctionOverload/Files/FunctionOverload.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+void printNum(float x)
+{
+   cout << x << endl;
+}
+void printNum(int x)
+{
+   cout << x << endl;
+}
+int main()
+{
+    int a = 10;
+    float b = 4.8777;
+    printNum(b);
+    printNum(a);
+    return 0;
+}
