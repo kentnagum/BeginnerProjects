ccb615e8eadff999e03fc77f1ea604295488dabc
Danielle <110608265+kentnagum@users.noreply.github.com>
2023-08-27 21:53:49 +0800

    Add files via upload

---
+++ stringOverwrite/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string phrase = "Hello World!";
+    cout << phrase << endl;
+    phrase = "Goodbye World!";
+    cout << phrase;
+    return 0;
+}
